dfb99744c54a5c8a1c12706c4754bff61f51ea58
赵智轩 <2024010667@secoder.net>
2026-08-07 20:03:44 +0800

    Reuse existing OpenVLA distributed process group

--- openvla_airplane/train.py
+++ openvla_airplane/train.py
@@ -49,7 +49,7 @@ def setup_distributed() -> tuple[int, int, bool]:
     world_size = int(os.environ.get("WORLD_SIZE", "1"))
     rank = int(os.environ.get("RANK", "0"))
     local_rank = int(os.environ.get("LOCAL_RANK", str(rank)))
-    if world_size > 1:
+    if world_size > 1 and not dist.is_initialized():
         dist.init_process_group("nccl")
     torch.cuda.set_device(local_rank)
     return rank, local_rank, world_size > 1
