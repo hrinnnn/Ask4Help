11ac8331be44529947ef88a51456dac1fbe1e019
赵智轩 <2024010667@secoder.net>
2026-08-15 20:24:16 +0800

    Raise StackPyramid blue safe waypoint

--- tools/collect_stackpyramid_xvla_dagger.py
+++ tools/collect_stackpyramid_xvla_dagger.py
@@ -245,7 +245,7 @@ class StackPyramidOracle:
                 break
         blue_position = moving_cube.pose.p.detach().cpu().numpy().reshape(-1, 3)[0]
         safe_pose = self.sapien.Pose(
-            [blue_position[0], blue_position[1], 0.20], grasp_pose.q
+            [blue_position[0], blue_position[1], 0.35], grasp_pose.q
         )
         self.planner.move_to_pose_with_screw(safe_pose)
         reach_pose = grasp_pose * self.sapien.Pose([0, 0, -0.05])
